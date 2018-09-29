12c1a33c36949de0b0112ece65893e79ac6842a9
Tak Castel <takcastel@gmail.com>
2018-09-29 15:39:54 +0200

    Import new component

--- plugins/global.js
+++ plugins/global.js
@@ -3,8 +3,10 @@ import VoBottomNav from '@/components/VoBottomNav'
 import VoPlayer from '@/components/VoPlayer'
 import VoPlayerControls from '@/components/VoPlayerControls'
 import VoPlayerMetadatas from '@/components/VoPlayerMetadatas'
+import VoPlayerLayout from '@/components/VoPlayerLayout'
 
 Vue.component('VoBottomNav', VoBottomNav)
 Vue.component('VoPlayer', VoPlayer)
 Vue.component('VoPlayerControls', VoPlayerControls)
 Vue.component('VoPlayerMetadatas', VoPlayerMetadatas)
+Vue.component('VoPlayerLayout', VoPlayerLayout)
