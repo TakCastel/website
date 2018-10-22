27d30766ea81692d5fba581607c0d548b3ccc71c
Tak Castel <takcastel@gmail.com>
2018-10-22 11:59:50 +0200

    Improve player progress controller

--- components/VoPlayerProgress/index.vue
+++ components/VoPlayerProgress/index.vue
@@ -1,16 +1,48 @@
 <template>
-  <v-slider
-    :class="{ overlapped: !isFullScreen }"
-  />
+  <v-card 
+    :class="{ 'progress_size hidden-xs-only': !isFullScreen }"
+    class="transparent mx-2"
+    flat>
+    <v-slider 
+      :value="value"
+      @change="handleChange"
+    />
+  </v-card>
 </template>
 
+<script>
+export default {
+  props: {
+    progress: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  computed: {
+    /**
+     * We pass the progress props as value
+     */
+    value: function () {
+      return this.progress
+    }
+  },
+
+  methods: {
+    /**
+     * Update the progress value in store
+     */
+    handleChange (sliderValue) {
+      this.$store.commit('updateCurrentTime', sliderValue)
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
-  .overlapped {
-    position: absolute;
-    top: -30px;
-    left: 0;
-    right: 0;
-    margin-left: 0;
-    margin-right: 0;
+  @media screen and (min-width: 600px) {
+    .progress_size {
+      flex: 1
+    }
   }
 </style>
